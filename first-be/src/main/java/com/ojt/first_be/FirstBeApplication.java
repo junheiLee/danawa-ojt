5f62dfa74af8c0a0d4ccd9a084de3e807d95d03d
junheiLee <jun2ee22@naver.com>
2024-06-05 16:10:55 +0900

    Feat: 프로젝트 생성

---
+++ first-be/src/main/java/com/ojt/first_be/FirstBeApplication.java
@@ -0,0 +1,13 @@
+package com.ojt.first_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstBeApplication.class, args);
+	}
+
+}
---
+++ first-be/src/test/java/com/ojt/first_be/FirstBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ojt.first_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
