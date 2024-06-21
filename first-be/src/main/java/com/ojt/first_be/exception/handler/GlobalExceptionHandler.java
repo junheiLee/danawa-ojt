9d795e11f8a485c001f28ae7f976b66bc8f880d2
junhei Lee <jun2ee22@naver.com>
2024-06-21 17:12:01 +0900

    Feat: 예외 처리 추가

--- first-be/src/main/java/com/ojt/first_be/exception/handler/GlobalExceptionHandler.java
+++ first-be/src/main/java/com/ojt/first_be/exception/handler/GlobalExceptionHandler.java
@@ -7,18 +7,33 @@ import com.ojt.first_be.exception.custom.NoExcelColumnAnnotationsException;
 import com.ojt.first_be.exception.custom.UnSupportedFileException;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.ooxml.POIXMLException;
+import org.apache.poi.util.RecordFormatException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.multipart.MultipartException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import static com.ojt.first_be.constant.ResultCode.NO_FILE;
+import static com.ojt.first_be.constant.ResultCode.*;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex,
+                                                                         HttpHeaders headers,
+                                                                         HttpStatusCode status,
+                                                                         WebRequest request) {
+        log.error("[SPRING EXCEPTION]={}", ex.getMessage());
+        return new ResponseEntity<>(new BaseResponse(SPRING_EXCEPTION), status);
+    }
 
     @ResponseStatus(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
     @ExceptionHandler(POIXMLException.class)
@@ -30,7 +45,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ResponseStatus(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
     @ExceptionHandler(UnSupportedFileException.class)
-    public BaseResponse excelHeaderExHandle(UnSupportedFileException e) {
+    public BaseResponse UnSupportedFileExHandle(UnSupportedFileException e) {
 
         log.error("[ExcelException] excel 외 파일 업로드 시도");
         return new BaseResponse(e.getResultCode());
@@ -38,7 +53,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
     @ExceptionHandler(NoExcelColumnAnnotationsException.class)
-    public BaseResponse excelHeaderExHandle(NoExcelColumnAnnotationsException e) {
+    public BaseResponse NoExcelColumnAnnotationsExHandle(NoExcelColumnAnnotationsException e) {
 
         log.error("[ExcelException] excel 파일 헤더와 업로드하고자 하는 도메인이 다름");
         return new BaseResponse(e.getResultCode());
@@ -46,7 +61,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(ExcelInternalException.class)
-    public void excelHeaderExHandle(ExcelInternalException e) {
+    public void ExcelInternalExHandle(ExcelInternalException e) {
 
         log.error("[ExcelException] 해당 없음", e);
     }
@@ -55,5 +70,11 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(MultipartException.class)
     public BaseResponse excelHandle() {
         return new BaseResponse(NO_FILE);
+    }
+
+    @ResponseStatus(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+    @ExceptionHandler(RecordFormatException.class)
+    public BaseResponse excelSizeExHandle() {
+        return new BaseResponse(TOO_BIG_SIZE);
     }
 }
